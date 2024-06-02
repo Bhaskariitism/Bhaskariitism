a7cbe59a8eb16f6c868e254d9983baa20fb421b7
Bhaskar <105628455+Bhaskariitism@users.noreply.github.com>
2024-06-02 23:30:46 +0530

    Add files via upload

---
+++ sumofn.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n:-\n");
+    scanf("%d",&n);
+    int sum = (n*(n+1))/2;
+    printf("The sum is:- %d\n", sum);
+    return 0;
+}
